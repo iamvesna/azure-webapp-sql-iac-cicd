a1848b878807db9f31782ecbe3d90fea37cd1acb
iamvesna <239870859+iamvesna@users.noreply.github.com>
2025-11-05 16:51:05 +0100

    Update providers.tf

--- infra/providers.tf
+++ infra/providers.tf
@@ -3,7 +3,7 @@
   required_providers { azurerm = { source = "hashicorp/azurerm", version = ">= 3.100.0" } }
   backend "azurerm" {
     resource_group_name  = "rg-tfstate-shared"
-    storage_account_name = "tfstatewebapp12345"   # <-- your unique name
+    storage_account_name = "tfstatewebapp12345"   
     container_name       = "tfstate"
     key                  = "project1/dev.tfstate"
   }
