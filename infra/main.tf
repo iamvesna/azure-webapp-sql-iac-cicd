5ee8c97acb97410531decb1b74415e4e18cc4173
Vesna <239870859+iamvesna@users.noreply.github.com>
2025-11-13 21:11:54 +0100

    infra: wire appservice module (web app + staging slot)

--- infra/main.tf
+++ infra/main.tf
@@ -6,4 +6,16 @@
   tags     = var.tags
 }
 
-output "resource_group" { value = module.core.rg_name }
+module "app" {
+  source    = "./modules/appservice"
+  rg_name   = module.core.rg_name
+  prefix    = var.prefix
+  env       = var.env
+  location  = var.location
+  tags      = var.tags
+  appi_cstr = module.core.appi_cstr
+}
+
+output "resource_group"       { value = module.core.rg_name }
+output "app_name"             { value = module.app.app_name }
+output "app_default_hostname" { value = module.app.app_default_hostname }
